202a95ee1ad5c9818f02302c690ba4e1436f180f
ShafayatAhammed <mdshafayat013@gmail.com>
2024-10-17 01:44:14 +0600

    Added necessary themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "normal-pmr": "#FB923C",
+        "high-pmr": "#F26922",
+        "normal-gray": "#A1A1AA",
+        "medium-gray": "#71717A",
+        "high-gray": "#27272A",
+        "normal-whitesmoke": "#F4F4F5",
+        "high-whitesmoke": "#E4E4E7",
+        "normal-dark": "#18181B",
+        "high-dark": "#09090B",
+      },
+      fontFamily: {
+        "bebas-neue": ["Bebas Neue", "sans-serif"],
+      },
+      screens: {
+        xlnav: "1200px",
+        xl4: "1700px",
+      },
+    },
   },
   plugins: [],
 };
