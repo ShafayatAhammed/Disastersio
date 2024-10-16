87c2d5fc80dff555087a2e14dbed3f040eba9cfb
ShafayatAhammed <mdshafayat013@gmail.com>
2024-10-17 01:44:50 +0600

    Added offcanvas component

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/header.jsx";
+import Offcanvas from "./components/offcanvas/offcanvas.jsx";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Offcanvas />
       <Outlet />
     </>
   );
