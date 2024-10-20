5b321d6d92d779402fda7c79e625697a9a263d3d
ShafayatAhammed <mdshafayat013@gmail.com>
2024-10-20 23:49:15 +0600

    New themes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,19 @@ export default {
         "normal-dark": "#18181B",
         "high-dark": "#09090B",
       },
-      fontFamily: {
-        "bebas-neue": ["Bebas Neue", "sans-serif"],
-      },
       screens: {
         xlnav: "1200px",
         xl4: "1700px",
         mdpghd: "850px",
         smpghd: "690px",
         xspghd: "450px",
+        mdincident: "950px",
+        smincident: "550px",
+        xsincident: "390px",
+        mdlocation: "700px",
+      },
+      boxShadow: {
+        chat: "0px 4px 48px 0px #0000000F",
       },
     },
   },
